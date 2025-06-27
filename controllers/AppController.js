c8e60ea00164257291eed9c270e3666a2eec351a
Arweenn <arwendumont25@outlook.fr>
2025-06-27 11:12:58 +0200

    2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,7 +5,7 @@ class AppController {
   static getStatus(req, res) {
     const redisAlive = redisClient.isAlive();
     const dbAlive = dbClient.isAlive();
-    
+
     res.status(200).json({
       redis: redisAlive,
       db: dbAlive
@@ -16,7 +16,7 @@ class AppController {
     try {
       const usersCount = await dbClient.nbUsers();
       const filesCount = await dbClient.nbFiles();
-      
+
       res.status(200).json({
         users: usersCount,
         files: filesCount
